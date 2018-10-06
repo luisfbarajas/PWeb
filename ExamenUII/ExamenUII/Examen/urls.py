3c0c004e0a639a2928f010d8af42332b5705d48a
Barajas <barajas@itt.com>
2018-10-06 16:25:48 -0700

    Vistas, URLs, Templates y estaticos agregados

--- ExamenUII/ExamenUII/Examen/urls.py
+++ ExamenUII/ExamenUII/Examen/urls.py
@@ -1,3 +1,17 @@
 from django.conf.urls import include, url
 from django.urls import path
 from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('fanatics/',views.fanatics, name='fanatics'),
+    path('fanaticsDetail/',views.fanaticsDetail,name='fanaticsDetail'),
+    path('fanaticsDates/',views.fanaticsDates,name='fanaticsDates'),
+    path('reports/',views.reports,name='reports'),
+    path('palcos/',views.palcos,name='palcos'),
+    path('palcosSearch/',views.palcosSearch,name='palcosSearch'),
+    path('PalcoFanatic/',views.palcoFanatics,name='palcoFanatic'),
+    path('seat/',views.seat,name='seat'),
+    path('seatOrderCategories/',views.seatOrderCategories,name='seatOrderCategories'),
+
+]
--- ExamenUII/ExamenUII/Examen/views.py
+++ ExamenUII/ExamenUII/Examen/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request,'ExamenTemplates/home.html')
+
+def fanatics(request):
+    return render(request,'ExamenTemplates/fanatics.html')
+
+def fanaticsDetail(request):
+    return render(request,'ExamenTemplates/fanaticsDetail.html')
+
+def fanaticsDates(request):
+    return render(request,'ExamenTemplates/fanaticsDates.html')
+
+def reports(request):
+    return render(request,'ExamenTemplates/reports.html')
+
+def palcos(request):
+    return render(request,'ExamenTemplates/palcos.html')
+
+def palcosSearch(request):
+    return render(request,'ExamenTemplates/palcosSearch.html')
+
+def palcoFanatics(request):
+    return render(request,'ExamenTemplates/PalcoFanatic.html')
+
+def seat(request):
+    return render(request,'ExamenTemplates/seat.html')
+
+def seatOrderCategories(request):
+    return render(request,'ExamenTemplates/seatOrderCategories.html')
