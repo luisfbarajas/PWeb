30b96f822322dac56a72f67852820d120323f181
luisfbarajas <luisf.barajas.btz@gamil.com>
2018-10-07 21:40:30 -0700

    Modelos y enlaces agregados

--- ExamenUII.2/ExamenU22/Examen/admin.py
+++ ExamenUII.2/ExamenU22/Examen/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Personal, Activities
 # Register your models here.
+admin.site.register(Personal)
+admin.site.register(Activities)
--- ExamenUII.2/ExamenU22/Examen/models.py
+++ ExamenUII.2/ExamenU22/Examen/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Activities(models.Model):
+    actName = models.CharField(max_length=255)
+    lead = models.CharField(max_length=255)
+    status = models.CharField(max_length=255)
+    start =  models.DateField()
+    end = models.DateField()
+
+    class Meta:
+        db_table='Activities'
+
+    def __str__(self):
+        return self.actName
+class Personal(models.Model):
+    name = models.CharField(max_length=255)
+    job = models.CharField(max_length=255)
+    jobTeam = models.CharField(max_length=255)
+    activitie = models.ForeignKey(Activities, on_delete = models.CASCADE)
+
+    class Meta:
+         db_table = 'Personal'
+
+    def __str__(self):
+        return self.name
+
