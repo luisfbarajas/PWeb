3cbd6ed4e34db05c26b967c27202b9e77365bf8c
luisfbarajas <luisf.barajas.btz@gamil.com>
2018-11-27 23:19:10 -0800

    Se agrega login logout y signup

--- ProyectoFinal/Proyecto/App/urls.py
+++ ProyectoFinal/Proyecto/App/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from . import views 
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path('login/', auth_views.LoginView.as_view(template_name = 'users/login.html'), name = 'login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name = 'users/logout.html'), name = 'logout'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(template_name = 'registration/logout.html'), name = 'logout'),
+    path('signup/', views.signup, name = 'signup')
 ]
